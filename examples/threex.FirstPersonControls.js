818b5b21c8405947af4ba0441dfdc42d898db5c9
erichlof <erichlof@swbell.net>
2014-02-11 15:44:56 -0600

    Update threex.FirstPersonControls.js

--- examples/threex.FirstPersonControls.js
+++ examples/threex.FirstPersonControls.js
@@ -16,21 +16,6 @@ THREEx.FirstPersonControls = function ( object, domElement ) {
 	this.lookSpeed = 0.005;
 
 	this.lookVertical = true;
-	this.autoForward = false;
-	// this.invertVertical = false;
-
-	this.activeLook = true;
-
-	this.heightSpeed = false;
-	this.heightCoef = 1.0;
-	this.heightMin = 0.0;
-	this.heightMax = 1.0;
-
-	this.constrainVertical = false;
-	this.verticalMin = 0;
-	this.verticalMax = Math.PI;
-
-	this.autoSpeedFactor = 0.0;
 
 	this.mouseX = 0;
 	this.mouseY = 0;
@@ -44,12 +29,14 @@ THREEx.FirstPersonControls = function ( object, domElement ) {
 	this.moveBackward = false;
 	this.moveLeft = false;
 	this.moveRight = false;
-	this.freeze = false;
-
-	this.mouseDragOn = false;
 
 	this.viewHalfX = 0;
 	this.viewHalfY = 0;
+	//moved
+	var actualMoveSpeed = 0;
+	var actualLookSpeed = 0;
+	var targetPosition = 0;
+	var position = 0;
 
 	if ( this.domElement !== document ) {
 
@@ -57,7 +44,6 @@ THREEx.FirstPersonControls = function ( object, domElement ) {
 
 	}
 
-	//
 
 	this.handleResize = function () {
 
@@ -78,28 +64,9 @@ THREEx.FirstPersonControls = function ( object, domElement ) {
 	
 	this.update = function( delta ) {
 
-		if ( this.freeze ) {
+		actualMoveSpeed = delta * this.movementSpeed;
 
-			return;
-
-		}
-
-		if ( this.heightSpeed ) {
-
-			var y = THREE.Math.clamp( this.object.position.y, this.heightMin, this.heightMax );
-			var heightDelta = y - this.heightMin;
-
-			this.autoSpeedFactor = delta * ( heightDelta * this.heightCoef );
-
-		} else {
-
-			this.autoSpeedFactor = 0.0;
-
-		}
-
-		var actualMoveSpeed = delta * this.movementSpeed;
-
-		if ( this.moveForward || ( this.autoForward && !this.moveBackward ) ) this.object.translateZ( - ( actualMoveSpeed + this.autoSpeedFactor ) );
+		if ( this.moveForward ) this.object.translateZ( - ( actualMoveSpeed ) );
 		if ( this.moveBackward ) this.object.translateZ( actualMoveSpeed );
 
 		if ( this.moveLeft ) this.object.translateX( - actualMoveSpeed );
@@ -108,37 +75,14 @@ THREEx.FirstPersonControls = function ( object, domElement ) {
 		if ( this.moveUp ) this.object.translateY( actualMoveSpeed );
 		if ( this.moveDown ) this.object.translateY( - actualMoveSpeed );
 
-		var actualLookSpeed = delta * this.lookSpeed;
 
-		if ( !this.activeLook ) {
-
-			actualLookSpeed = 0;
-
-		}
-
-		var verticalLookRatio = 1;
-
-		if ( this.constrainVertical ) {
-
-			verticalLookRatio = Math.PI / ( this.verticalMax - this.verticalMin );
-
-		}
-
-		//this.lon += this.mouseX * actualLookSpeed;
-		//if( this.lookVertical ) this.lat -= this.mouseY * actualLookSpeed * verticalLookRatio;
 		this.lat = Math.max( - 85, Math.min( 85, this.lat ) );
 		
 		this.phi = THREE.Math.degToRad( 90 - this.lat );
 		this.theta = THREE.Math.degToRad( this.lon );
 
-		if ( this.constrainVertical ) {
-
-			this.phi = THREE.Math.mapLinear( this.phi, 0, Math.PI, this.verticalMin, this.verticalMax );
-
-		}
-
-		var targetPosition = this.target,
-			position = this.object.position;
+		targetPosition = this.target;
+		position = this.object.position;
 
 		targetPosition.x = position.x + Math.sin( this.phi ) * Math.cos( this.theta ); //position.x + 100
 		targetPosition.y = position.y + Math.cos( this.phi );//position.y + 100
